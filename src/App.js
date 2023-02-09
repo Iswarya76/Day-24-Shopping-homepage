cde7d072e4830bbc0a4d81e57aae06cdc75868b5
Iswarya76 <babumohan394@gmail.com>
2023-02-10 00:44:45 +0530

    Will be continued

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[products,setProducts]=useState([
+    {
+      id:1,
+      name:'Apple iPhone 12',
+      price: 79900, 
+      image: 'https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662702995/Croma%20Assets/Communication/Mobiles/Images/261943_ffc6cl.png/mxw_2048,f_auto'
+    },
+    {
+      id:2,
+      name:'Samsung Galaxy A71',
+      price: 22000,
+      image:'https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1666992553/Croma%20Assets/Communication/Mobiles/Images/233768_qn7m8w.png/mxw_1440,f_auto'
+    },
+    {
+      id:3,
+      name:'OnePlus 10 Pro',
+      price:61000,
+      image:'https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1666992010/Croma%20Assets/Communication/Mobiles/Images/250719_t2svgl.png'
+    },
+    {
+      id:4,
+      name:'Xiomi 10',
+      price:12000,
+      image:'https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662438329/Croma%20Assets/Communication/Mobiles/Images/251042_cjw7wb.png/mxw_1440,s_webp,f_auto'
+    },
+    {
+      id:5,
+      name:'Nokia G31',
+      price:14000,
+      image:'https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662435270/Croma%20Assets/Communication/Mobiles/Images/251584_hdcuw4.png/mxw_1440,s_webp,f_auto'
+    }
+  ])
+  const[cartItems,setCartItems]=useState([])
+  let addToCart=(product)=>{
+    setCartItems([...cartItems,products])
+  }
+  let removeFromCart=(cartItem)=>{ 
+    const indexVal=cartItems.findIndex(obj=>obj.id===cartItem.id); 
+    cartItems.splice(indexVal,1); 
+    setCartItems([...cartItems]) 
+  } 
   return (
     
   );
